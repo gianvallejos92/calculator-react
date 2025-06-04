f227fb1156520782470b7537d5ed55fc762cda2d
Gian Vallejos <gian.vallejos92@gmail.com>
2025-06-04 16:18:54 -0500

    calculator negative numbers, delete and percent

--- src/reducers/calculateOperation.js
+++ src/reducers/calculateOperation.js
@@ -12,6 +12,12 @@ const isValidScreenLimit = () => {
     return global.curValue && global.curValue.length === MAX_LENGTH;
 }
 
+const itsNegativeSign = (str, ind) => {
+    if (ind === 0 && ind + 1 != str.length && Number(str[ind+1])) return true;
+    if (ind > 0 && ind + 1 != str.length && Number(str[ind+1]) && OPERATORS.includes(str[ind-1])) return true;
+    return false;ß
+}
+
 const splitNumbersAndSigns = (str) => {
     let lastInit = 0;
     let numbers = [];
@@ -19,16 +25,18 @@ const splitNumbersAndSigns = (str) => {
     let error = false;
     for (let ind = 0; ind < str.length; ind++) {
         if (OPERATORS.includes(str[ind])) {
-            let aux = str.slice(lastInit, ind);
-            if (!Number(aux)) {
-                return {
-                    "numbers": ['error'],
-                    "operators": ['error']
+            if (!itsNegativeSign(str, ind)) {
+                let aux = str.slice(lastInit, ind);
+                if (!Number(aux)) {
+                    return {
+                        "numbers": ['error'],
+                        "operators": ['error']
+                    }
                 }
+                numbers.push(Number(aux));
+                operators.push(str[ind]);
+                lastInit = ind + 1;
             }
-            numbers.push(Number(aux));
-            operators.push(str[ind]);
-            lastInit = ind + 1;
         }
     }
     if(!error && lastInit != 0) {
@@ -48,7 +56,7 @@ const executeOperation = (str) => {
     //validate not operators together
     let result = splitNumbersAndSigns(str);
 
-    console.log('result: ' + result);
+    console.log('result: ' + JSON.stringify(result));
 
     if (result.numbers.length != result.operators.length + 1) return "error";
 
@@ -85,19 +93,36 @@ function calculateResult () {
     //CONVER TO SWITCH + CLEAN
     if (global.newValue === 'AC') {
         global.result = "0"; return;
-    } else if (global.newValue === '=') { //Resolve operations
-        if (!Number(global.curValue)) {
+    } else if (global.newValue === 'CE') {
+        if (global.curValue == "0" || global.curValue.length === 1) {
+            global.result = "0";
+        } else {
+            let screenValue = String(global.curValue);
+            global.result = screenValue.slice(0, screenValue.length - 1);
+        }
+    } else if (global.newValue === '%') {
+        if (Number(global.curValue)) {
+            global.result = String(Number(global.curValue) / 100);
+        } else {
+            global.result = global.curValue;
+        }
+    } else if (global.newValue === '=') {
+        if (global.curValue === "0" ) {
+            global.result = "0";
+        } else if (!Number(global.curValue)) {
             global.result = executeOperation(global.curValue);
         } else {
             global.result = global.curValue;
         }
-    } else if (global.curValue == "0") {
-        global.result = global.newValue;
-    } else if (isValidScreenLimit()) {
-        global.result = global.curValue;
     } else {
-         global.result = global.curValue + global.newValue; 
-    }  
+        if (global.curValue == "0") {
+            global.result = global.newValue;
+        } else if (isValidScreenLimit()) {
+            global.result = global.curValue;
+        } else {
+            global.result = global.curValue + global.newValue; 
+        }  
+    }
 }
 
 export const calculatorOperation = (newValue, curValue) => {
